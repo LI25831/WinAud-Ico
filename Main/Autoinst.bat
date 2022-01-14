c0c850b7d677e15efb2b5f94b115dc249a54eec5
李筱涵 <72011083+LI25831@users.noreply.github.com>
2022-01-14 18:26:24 +0800

    Update Autoinst.bat

--- Main/Autoinst.bat
+++ Main/Autoinst.bat
@@ -1,5 +1,6 @@
 @echo off
-Xcopy explorer C:\Users\Public\AppData\Local\Themes\Pack1\ /S
-reg EXPORT HKEY_CURRENT_USER\AppEvents C:\Users\Public\AppData\Local\Backup.reg
-explorer C:\Users\Public\AppData\Local\Themes\Pack1\
+Xcopy D:\Main C:\Users\Public\AppData\Local\ /S
+reg EXPORT HKEY_CURRENT_USER\AppEvents C:\Users\Public\AppData\Local\Win7SysAud\Backup.reg
+reg IMPORT C:\Users\Public\AppData\Local\1.reg
+explorer C:\Users\Public\AppData\Local\
 Pause
