1e7dfa2a1f51f100c377af24d53f2cb263cc5f6f
李筱涵 <72011083+LI25831@users.noreply.github.com>
2022-01-14 18:15:12 +0800

    RE add winthemepack support

--- Main/Autoinst.bat
+++ Main/Autoinst.bat
@@ -1,5 +1,5 @@
 @echo off
-Xcopy D:\Main C:\Users\Public\AppData\Local\ /S
+Xcopy explorer C:\Users\Public\AppData\Local\Themes\Pack1\ /S
 reg EXPORT HKEY_CURRENT_USER\AppEvents C:\Users\Public\AppData\Local\Backup.reg
-reg IMPORT C:\Users\Public\AppData\Local\1.reg
+explorer C:\Users\Public\AppData\Local\Themes\Pack1\
 Pause
