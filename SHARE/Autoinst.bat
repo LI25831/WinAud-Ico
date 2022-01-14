bcdce3590f8195621c4bafd84e5173c421e9b49f
李筱涵 <72011083+LI25831@users.noreply.github.com>
2022-01-14 15:28:53 +0800

    clear old files
    CC

--- SHARE/Autoinst.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REG IMPORT D:\SHARE\Dependence\1.reg
-REG IMPORT D:\SHARE\Dependence\2.reg
-XCOPY D:\SHARE\Dependence\SysAud C:\USERS\LiXH\AppData\Local\SysAud /S
-XCOPY D:\SHARE\Dependence\Sysicon C:\Users\LiXH\AppData\Local\Sysicon /S
-pause
